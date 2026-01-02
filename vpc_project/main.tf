7f0691a8c1c7d6ff55662d65f4cc137942b768fc
Abhi <abhitarlada2003@gmail.com>
2026-01-02 11:43:28 +0530

    Added VPC and Subnet setup for main.tf

---
+++ vpc_project/main.tf
@@ -0,0 +1,13 @@
+#creating VPC
+
+resource "aws_vpc" "myvpc" {
+ cidr_block = var.cidr
+}
+
+#Creating Subnets
+resource "aws_subnet" "sub1"{
+   vpc_id                   = aws_vpc.myvpc.id
+   cidr_block               = "192.168.1.0/24"
+   availability_zone        = "ap-south-1a"
+   map_public_ip_on_launch  = true
+}
